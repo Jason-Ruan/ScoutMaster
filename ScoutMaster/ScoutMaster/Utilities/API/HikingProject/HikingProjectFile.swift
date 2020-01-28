9e944ccfb7207360c3ed479bfe43cbf14c9689ca
Aaron Pachesa <aaronpachesa@gmail.com>
2020-01-28 15:29:11 -0500

    added model

--- ScoutMaster/ScoutMaster/Utilities/API/HikingProject/HikingProjectFile.swift
+++ ScoutMaster/ScoutMaster/Utilities/API/HikingProject/HikingProjectFile.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct Trails {
+    var trail: [Trail]
+}
+
+struct Trail: Codable {
+    var id: Int
+    var name: String
+    var type: String
+    var summary: String
+    var difficulty: String
+    var stars: Int
+    var starVotes: Int
+    var location: String
+    var url: String
+    var imgSqSmall: String
+    var imgSmall: String
+    var ingSmallMed: String
+    var imgMedium: String
+    var length: Double
+    var ascent: Int
+    var descent: Int
+    var high: Int
+    var low: Int
+    var longitude: Double
+    var latitude: Double
+    var conditionStatus: String
+    var conditionDetails: String?
+    var conditionDate: String
+}
