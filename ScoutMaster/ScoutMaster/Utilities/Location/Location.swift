9b0843147c57bccc88b82b9e27ff869a084b2978
Sam Roman <samroman@Sams-MacBook-Pro.local>
2020-02-03 11:52:44 -0500

    Adds custompointannotation file in location folder

--- ScoutMaster/ScoutMaster/Utilities/Location/Location.swift
+++ ScoutMaster/ScoutMaster/Utilities/Location/Location.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2020 Sam Roman. All rights reserved.
 //
 
+
 import Foundation
+
+// MARK: - LocationData
+struct LocationData: Codable {
+    let type: String?
+    let features: [Feature]?
+    
+    static func getCoordinatesFromData(data: Data) throws -> [[Double]]? {
+    do {
+     let info = try JSONDecoder().decode(LocationData.self, from: data)
+        return info.features?[0].geometry?.coordinates
+
+    } catch {
+        print(error)
+        return []
+    }
+    }
+}
+
+// MARK: - Feature
+struct Feature: Codable {
+    let type: String?
+    let properties: Properties?
+    let geometry: Geometry?
+}
+
+// MARK: - Geometry
+struct Geometry: Codable {
+    let type: String?
+    let coordinates: [[Double]]?
+}
+
+// MARK: - Properties
+struct Properties: Codable {
+    let name: String?
+}
+
