285e119600b9f6a14fdd5060edd6d752329af8c9
Jason Ruan <jasonruan@pursuit.org>
2020-02-10 11:15:09 -0500

    Adds Mapbox methods and mapview delegate methods from MapVC to DetailVC

--- ScoutMaster/ScoutMaster/Controllers/Main Tab/Dashboard/DetailVC.swift
+++ ScoutMaster/ScoutMaster/Controllers/Main Tab/Dashboard/DetailVC.swift
@@ -160,4 +160,84 @@ class DetailVC: UIViewController {
 }
 
 
+//MARK: Mapbox Methods
+extension DetailVC: MGLMapViewDelegate {
+    
+    func getCoordinates(data: Data){
+        do {
+            var coords = [(Double,Double)]()
+            let rawCoords = (try LocationData.getCoordinatesFromData(data: data))!
+            for i in rawCoords {
+                coords.append((i[1],i[0]))
+            }
+            newCoords = coords
+        }
+        catch {
+            print("error, could not decode geoJSON")
+        }
+    }
+    
+    
+    
+    func drawTrailPolyline() {
+        // Parsing GeoJSON can be CPU intensive, do it on a background thread
+        DispatchQueue.global(qos: .background).async(execute: {
+            // Get the path for example.geojson in the app's bundle
+            let jsonPath = Bundle.main.path(forResource: "blue-trail", ofType: "geojson")
+            let url = URL(fileURLWithPath: jsonPath!)
+            
+            do {
+                // Convert the file contents to a shape collection feature object
+                let data = try Data(contentsOf: url)
+                self.getCoordinates(data: data)
+                guard let shapeCollectionFeature = try MGLShape(data: data, encoding: String.Encoding.utf8.rawValue) as? MGLShapeCollectionFeature else {
+                    fatalError("Could not cast to specified MGLShapeCollectionFeature")
+                }
+                
+                if let polyline = shapeCollectionFeature.shapes.first as? MGLPolylineFeature {
+                    // Optionally set the title of the polyline, which can be used for:
+                    //  - Callout view
+                    //  - Object identification
+                    polyline.title = polyline.attributes["name"] as? String
+                    // Add the annotation on the main thread
+                    DispatchQueue.main.async(execute: {
+                        // Unowned reference to self to prevent retain cycle
+                        [unowned self] in
+                        self.mapView.addAnnotation(polyline)
+                    })
+                }
+                
+            } catch {
+                print("GeoJSON parsing failed")
+            }})
+    }
+    
+    //MARK: - MV Delegates
+    func mapView(_ mapView: MGLMapView, alphaForShapeAnnotation annotation: MGLShape) -> CGFloat {
+        return 0.8
+    }
+    
+    func mapView(_ mapView: MGLMapView, lineWidthForPolylineAnnotation annotation: MGLPolyline) -> CGFloat {
+        // Set the line width for polyline annotations
+        return 4.0
+    }
+    
+    func mapView(_ mapView: MGLMapView, strokeColorForShapeAnnotation annotation: MGLShape) -> UIColor {
+        
+        // Give our polyline a unique color by checking for its `title` property
+        if annotation is MGLPolyline {
+            switch annotation.title {
+            case "Blue Trail":
+                return .systemBlue
+            default:
+                return .white
+            }
+        }
+        return .white
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
 }
