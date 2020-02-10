d8c79035678e2cb18f4146c70c8d866a6bbe57e8
Sam Roman <samroman@Sams-MacBook-Pro.local>
2020-02-10 12:37:02 -0500

    Adds collectionview to mapvc, mapsettings file to utilities

---
+++ ScoutMaster/ScoutMaster/Utilities/Location/MapSettings.swift
@@ -0,0 +1,49 @@
+//
+//  MapSettings.swift
+//  mapBoxTester
+//
+//  Created by Sam Roman on 2/3/20.
+//  Copyright © 2020 Sam Roman. All rights reserved.
+//
+
+import Foundation
+import Mapbox
+
+class MapSettings {
+    
+    static func toggleMapStyle(mapView: MGLMapView ) -> UIAlertController {
+    let actionSheet = UIAlertController(title: "Choose map style", message: nil, preferredStyle: .actionSheet)
+    
+    let lightStyle = UIAlertAction(title: "Light", style: .default) { (action) in
+        mapView.styleURL = MGLStyle.lightStyleURL
+    }
+    
+    let darkStyle = UIAlertAction(title: "Dark", style: .default) { (action) in
+        mapView.styleURL = MGLStyle.darkStyleURL
+    }
+    
+    let satelliteStyle = UIAlertAction(title: "Satellite", style: .default) { (action) in
+        mapView.styleURL = MGLStyle.satelliteStyleURL
+    }
+    
+    let outdoorsStyle = UIAlertAction(title: "Outdoors", style: .default) { (action) in
+        mapView.styleURL = MGLStyle.outdoorsStyleURL
+    }
+    
+    let streetStyle = UIAlertAction(title: "Street", style: .default) { (action) in
+       mapView.styleURL = MGLStyle.streetsStyleURL
+    }
+    
+    let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    
+    actionSheet.addAction(lightStyle)
+    actionSheet.addAction(darkStyle)
+    actionSheet.addAction(satelliteStyle)
+    actionSheet.addAction(outdoorsStyle)
+    actionSheet.addAction(streetStyle)
+    actionSheet.addAction(cancel)
+        
+    return actionSheet
+}
+
+}
