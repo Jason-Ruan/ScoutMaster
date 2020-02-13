0a0af5960a50b63d471c09819002bb53c1b32e9f
Aaron Pachesa <aaronpachesa@gmail.com>
2020-02-13 12:24:41 -0500

    added to secrets, added google geocode api file

--- ScoutMaster/ScoutMaster/Supporting Files/SceneDelegate.swift
+++ ScoutMaster/ScoutMaster/Supporting Files/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = LoginVC()
+        window?.rootViewController = DashboardVC()
         
         /*
         if FirebaseAuthService.manager.currentUser != nil {
