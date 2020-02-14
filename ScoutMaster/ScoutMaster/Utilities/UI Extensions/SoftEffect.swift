9099bee6138be67fbbe7becc8d43c6e645078078
samroman3 <samuelroman@pursuit.org>
2020-02-13 21:39:30 -0500

    Adds placeholder profile image to assets and adds UI Extension folder to Utilities

---
+++ ScoutMaster/ScoutMaster/Utilities/UI Extensions/SoftEffect.swift
@@ -0,0 +1,33 @@
+//
+//  SoftEffect.swift
+//  ScoutMaster
+//
+//  Created by Sam Roman on 2/13/20.
+//  Copyright © 2020 Sam Roman. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+extension UIView {
+    
+    public func addSoftUIEffectForView(cornerRadius: CGFloat = 15.0, themeColor: UIColor = UIColor(red: 241/255, green: 243/255, blue: 246/255, alpha: 1.0)) {
+        self.layer.cornerRadius = cornerRadius
+        self.layer.masksToBounds = false
+        self.layer.shadowRadius = 2
+        self.layer.shadowOpacity = 1
+        self.layer.shadowOffset = CGSize( width: 2, height: 2)
+        self.layer.shadowColor = UIColor(red: 223/255, green: 228/255, blue: 238/255, alpha: 1.0).cgColor
+        
+        let shadowLayer = CAShapeLayer()
+        shadowLayer.frame = bounds
+        shadowLayer.backgroundColor = themeColor.cgColor
+        shadowLayer.shadowColor = UIColor.white.cgColor
+        shadowLayer.cornerRadius = cornerRadius
+        shadowLayer.shadowOffset = CGSize(width: -2.0, height: -2.0)
+        shadowLayer.shadowOpacity = 1
+        shadowLayer.shadowRadius = 2
+        self.layer.insertSublayer(shadowLayer, at: 0)
+    }
+}
