d344e8efeadfefa06d108cc0d26c01c348937f9a
Jason Ruan <jasonruan@pursuit.org>
2020-03-09 16:06:35 -0400

    Adds WeatherCell to DarkSky API folder

---
+++ ScoutMaster/ScoutMaster/Utilities/API/DarkSky/WeatherCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherCell.swift
+//  ScoutMaster
+//
+//  Created by Jason Ruan on 3/6/20.
+//  Copyright © 2020 Sam Roman. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    
+}
