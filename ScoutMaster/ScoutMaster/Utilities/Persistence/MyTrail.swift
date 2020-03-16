30146f9c5dc3823f7b3a9417af8852298b89bf65
samroman3 <samuelroman@pursuit.org>
2020-03-16 14:20:52 -0400

    Adds persistence helper for MyTrail file, adds uialerts for recording status

--- ScoutMaster/ScoutMaster/Utilities/Persistence/MyTrail.swift
+++ ScoutMaster/ScoutMaster/Utilities/Persistence/MyTrail.swift
@@ -7,6 +7,24 @@
 //
 
 import Foundation
+import CoreLocation
+
 struct MyTrail: Codable {
+    var title: String
+    var trailID: String
+    var creatorID: String
+    var description: String?
+    var images: [Data]
+    var difficulty: String
+    var coordinate: [String]
     
+    
+    func getTrueCoordinates() -> [(Double,Double)] {
+        var trueCoords = [(Double,Double)]()
+        for i in coordinate {
+            let components = i.components(separatedBy: ",")
+            trueCoords.append(((Double(components[0])!), (Double(components[1])!)))
+        }
+        return trueCoords
+    }
 }
