a56111702cffb764dc4b271b6ac1a73853e37184
Jason Ruan <jasonruan@pursuit.org>
2020-03-24 16:26:55 -0400

    Adds weather caching to DarkSkyFile

--- ScoutMaster/ScoutMaster/Utilities/API/DarkSky/DarkSkyFile.swift
+++ ScoutMaster/ScoutMaster/Utilities/API/DarkSky/DarkSkyFile.swift
@@ -2,8 +2,8 @@
 //  DarkSkyFile.swift
 //  ScoutMaster
 //
-//  Created by Aaron Pachesa on 1/28/20.
-//  Copyright © 2020 Sam Roman. All rights reserved.
+//  Created by Jason Ruan on 1/28/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
 //
 
 import Foundation
@@ -63,97 +63,107 @@ class DarkSkyAPIClient {
     private init() {}
     static let manager = DarkSkyAPIClient()
     
+    let cachedWeather = NSCache<NSString, WeatherForecast>()
+    
     // Gets back WeatherForecast object with both daily and hourly forecasts
     func fetchWeatherForecast(lat: Double, long: Double, completionHandler: @escaping (Result<WeatherForecast, AppError>) -> () ) {
         
         let urlStr = "https://api.darksky.net/forecast/\(Secrets.darkSky_key)/\(lat),\(long)"
         
-        NetworkManager.shared.fetchData(urlString: urlStr, completionHandler: { (result) in
-            switch result {
-            case .failure(let error):
-                completionHandler(.failure(error))
-            case .success(let data):
-                do {
-                    let darkSkyResult = try JSONDecoder().decode(WeatherForecast.self, from: data)
-                    guard let _ = darkSkyResult.daily, let _ = darkSkyResult.hourly else {
-                        completionHandler(.failure(.badJSONError))
-                        return
+        if let cachedWeather = cachedWeather.object(forKey: urlStr as NSString) {
+            // Check cache before making network call
+            completionHandler(.success(cachedWeather))
+        } else {
+            // Object was not found in cache, so make a network call
+            print(urlStr)
+            NetworkManager.shared.fetchData(urlString: urlStr, completionHandler: { (result) in
+                switch result {
+                    case .failure(let error):
+                        completionHandler(.failure(error))
+                    case .success(let data):
+                        do {
+                            let darkSkyResult = try JSONDecoder().decode(WeatherForecast.self, from: data)
+                            guard let _ = darkSkyResult.daily, let _ = darkSkyResult.hourly else {
+                                completionHandler(.failure(.badJSONError))
+                                return
+                            }
+                            //Cache WeatherForecast object for future use and return it
+                            self.cachedWeather.setObject(darkSkyResult, forKey: urlStr as NSString)
+                            completionHandler(.success(darkSkyResult))
+                        } catch let error {
+                            completionHandler(.failure(.other(errorDescription: error.localizedDescription)))
                     }
-                    completionHandler(.success(darkSkyResult))
-                } catch let error {
-                    completionHandler(.failure(.other(errorDescription: error.localizedDescription)))
                 }
-            }
-        })
+            })
+        }
+        
     }
-    
 }
-
-//MARK: - Weather Forecast Object w/ Daily and Hourly
-
-struct WeatherForecast: Codable {
-    let timezone: String?
-    let daily: DailyForecast?
-    let hourly: HourlyForecast?
+    //MARK: - Weather Forecast Object w/ Daily and Hourly
     
-    struct DailyForecast: Codable {
-        let summary: String?
-        let icon: String?
-        let data: [DayForecastDetails]?
-        let alerts: [AlertsWrapper]?
+    class WeatherForecast: Codable {
+        let timezone: String?
+        let daily: DailyForecast?
+        let hourly: HourlyForecast?
+        
+        struct DailyForecast: Codable {
+            let summary: String?
+            let icon: String?
+            let data: [DayForecastDetails]?
+            let alerts: [AlertsWrapper]?
+        }
+        
+        struct HourlyForecast: Codable {
+            let summary: String?
+            let icon: String?
+            let data: [HourForecastDetails]?
+        }
+        
+        struct AlertsWrapper: Codable {
+            let title: String?
+            let time: Int?
+            let expires: Int?
+            let descriptiption: String?
+            let uri: String?
+        }
+        
+        
     }
-
-    struct HourlyForecast: Codable {
-        let summary: String?
-        let icon: String?
-        let data: [HourForecastDetails]?
-    }
-
-    struct AlertsWrapper: Codable {
-        let title: String?
+    
+    //MARK: - Daily Forecast Details
+    
+    struct DayForecastDetails: Codable {
         let time: Int?
-        let expires: Int?
-        let descriptiption: String?
-        let uri: String?
+        let summary: String?
+        let icon: String?
+        let sunriseTime: Int?
+        let sunsetTime: Int?
+        let precipIntensityMax: Double?
+        let precipProbability: Double?
+        let precipType: String?
+        let temperatureHigh: Double?
+        let temperatureLow: Double?
+        let apparentTemperatureHigh: Double?
+        let windSpeed: Double?
+        let humidity: Double?
+        let cloudCover: Double?
+        let visibility: Double?
     }
-
     
-}
-
-//MARK: - Daily Forecast Details
-
-struct DayForecastDetails: Codable {
-    let time: Int?
-    let summary: String?
-    let icon: String?
-    let sunriseTime: Int?
-    let sunsetTime: Int?
-    let precipIntensityMax: Double?
-    let precipProbability: Double?
-    let precipType: String?
-    let temperatureHigh: Double?
-    let temperatureLow: Double?
-    let apparentTemperatureHigh: Double?
-    let windSpeed: Double?
-    let humidity: Double?
-    let cloudCover: Double?
-    let visibility: Double?
-}
-
-
-//MARK: - Hourly Forecast Details
-
-struct HourForecastDetails: Codable {
-    let time: Int?
-    let summary: String?
-    let icon: String?
-    let precipIntensity: Double?
-    let precipProbability: Double?
-    let precipType: String?
-    let temperature: Double?
-    let apparentTemperature: Double?
-    let humidity: Double?
-    let windSpeed: Double?
-    let cloudCover: Double?
-    let visibility: Double?
+    
+    //MARK: - Hourly Forecast Details
+    
+    struct HourForecastDetails: Codable {
+        let time: Int?
+        let summary: String?
+        let icon: String?
+        let precipIntensity: Double?
+        let precipProbability: Double?
+        let precipType: String?
+        let temperature: Double?
+        let apparentTemperature: Double?
+        let humidity: Double?
+        let windSpeed: Double?
+        let cloudCover: Double?
+        let visibility: Double?
 }
