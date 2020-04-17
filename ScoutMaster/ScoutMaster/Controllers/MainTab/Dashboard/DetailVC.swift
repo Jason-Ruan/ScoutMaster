7962dc4c38a293682e93e78794aad61d84b39ca9
Jason Ruan <jasonruan@pursuit.org>
2020-04-17 11:07:33 -0400

    Removes header from DetailVC

--- ScoutMaster/ScoutMaster/Controllers/MainTab/Dashboard/DetailVC.swift
+++ ScoutMaster/ScoutMaster/Controllers/MainTab/Dashboard/DetailVC.swift
@@ -1,11 +1,3 @@
-//
-//  Dashboard.swift
-//  ScoutMaster
-//
-//  Created by Sam Roman on 1/27/20.
-//  Copyright © 2020 Sam Roman. All rights reserved.
-//
-
 import UIKit
 import Mapbox
 import SafariServices
