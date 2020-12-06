4e46ad575d5b5049baaf9220deef6e3341bc521a
Jason Ruan <jasonruan@pursuit.org>
2020-12-06 00:00:03 -0500

    Adds 2 unit tests involving API calls

--- ScoutMaster/ScoutMasterTests/ScoutMasterTests.swift
+++ ScoutMaster/ScoutMasterTests/ScoutMasterTests.swift
@@ -19,15 +19,50 @@ class ScoutMasterTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHikingProjectFetchTrails() {
+        let expectation = self.expectation(description: "Asynchronous call to HikingProject API completed successfully with at least 1 trail returned")
+    
+        Trail.getTrails(lat: 40.7043, long: -73.854) { (result) in
+            switch result {
+                case .failure(let error):
+                    XCTFail("HikingProject API call for trails did not return a valid result, error: \(error)")
+                case .success(let trailResults):
+                    XCTAssert(trailResults.count > 0, "HikingProject API should return trails")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5) { (error) in
+            if let error = error {
+                XCTFail("There was a timeout error while trying to get response from HikingProject API, error: \(error)")
+            }
+        }
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testDarkSkyFetchWeather() {
+        let expectation = self.expectation(description: "Asynchronous call to DarkSky API completed successfully with weather forecast results")
+    
+        DarkSkyAPIClient.manager.fetchWeatherForecast(lat: 40.7043, long: -73.854) { (result) in
+            switch result {
+                case .failure(let error):
+                    XCTFail("DarkSky API call for weather forecast did not return a valid result, error: \(error)")
+                    
+                case .success(let weatherForecast):
+                    guard let hourlyForecast = weatherForecast.hourly?.data, let dailyForecast = weatherForecast.daily?.data else {
+                        return XCTFail("DarkSky API result did not include data for hourly and daily forecasts")
+                    }
+                    XCTAssert(hourlyForecast.count > 0 && dailyForecast.count > 0, "DarkSky API should return both hourly and daily forecast data")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5) { (error) in
+            if let error = error {
+                XCTFail("There was a timeout error while trying to get response from DarkSky API, error: \(error)")
+            }
         }
     }
 
